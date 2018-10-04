b4eaf54bb07e6ecc19b06f8fed253758244589b2
DAOXIANG DENG <38935285@qq.com>
2018-10-04 17:15:57 +0800

    Initial Commit

---
+++ 老易养/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  老易养
+//
+//  Created by Ryan on 2018/10/4.
+//  Copyright © 2018 ChuanQiXiongDi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ 老易养/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  老易养
+//
+//  Created by Ryan on 2018/10/4.
+//  Copyright © 2018 ChuanQiXiongDi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
